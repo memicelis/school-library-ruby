72142a63ad97f511520001028f8911d9cc2987b2
memicelis <elismemic5@gmail.com>
2023-11-08 13:17:46 +0100

    Update Student class with add classroom method

--- student.rb
+++ student.rb
@@ -4,8 +4,13 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission:)
     @classroom = classroom
+  end
+
+  def add_classroom
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
