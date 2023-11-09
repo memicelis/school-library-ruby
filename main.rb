705397e7ec913d23dde526a80dc8a0df1e558b91
memicelis <elismemic5@gmail.com>
2023-11-09 15:31:48 +0100

    Created App and Main classes

---
+++ main.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require_relative 'app'
+
+def main
+  app = App.new
+
+  loop do
+    puts "Options:"
+    puts "1. List all books"
+    puts "2. List all people"
+    puts "3. Create a person"
+    puts "4. Create a book"
+    puts "5. Create a rental"
+    puts "6. List rentals for a person"
+    puts "7. Quit"
+
+    choice = gets.chomp
+
+    case choice
+    when '1'
+      app.list_all_books
+    when '2'
+      app.list_all_people
+    when '3'
+      app.create_person
+    when '4'
+      app.create_book
+    when '5'
+      app.create_rental
+    when '6'
+      app.list_rentals
+    when '7'
+      puts "Exiting the app. Goodbye!"
+      break
+    else
+      puts "Invalid choice. Please enter a valid option."
+    end
+  end
+end
+
+# Invoke the main method
+main
