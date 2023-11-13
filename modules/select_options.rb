2e4ecdcfd74c2c1a1b1805d60bc796d11c47a76f
memicelis <elismemic5@gmail.com>
2023-11-13 14:27:48 +0100

    Created modules for BookHandler,RentalHandler, PeopleHandler, SelectOptions, UserInputHandler

---
+++ modules/select_options.rb
@@ -0,0 +1,20 @@
+module SelectOptions
+    def selected_option(option)
+        case option
+        when '1'
+          list_all_books
+        when '2'
+          list_all_people
+        when '3'
+          create_person
+        when '4'
+          create_book
+        when '5'
+          create_rental
+        when '6'
+          list_rentals
+        else
+          puts 'Please select valid option from the list!\n'
+        end
+    end
+end
---
+++ modules/user_input_handler.rb
@@ -0,0 +1,10 @@
+module UserInputHandler
+    def get_user_input(prompt)
+      puts prompt
+      gets.chomp
+    end
+  
+    def get_user_input_as_integer(prompt)
+      get_user_input(prompt).to_i
+    end
+end
